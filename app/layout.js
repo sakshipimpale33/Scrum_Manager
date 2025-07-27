bb513992cc31f2463c27985703327c1ca9946f92
Sakshi Pimpale <sakshipimpale88@gmail.com>
2025-07-27 13:58:08 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
             <Toaster richColors />
             <footer className="bg-gray-900 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
+                
               </div>
             </footer>
           </ThemeProvider>
